72dc48a2c304faf6a687034c6d253199d8134b20
Kitan Garcia <kitangar@gmail.com>
2023-03-18 12:40:06 -0700

    remove mui; start using daisyui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex w-max">
+      <h1 className="text-5xl my-12">Plant Arrangement Calculator</h1>
+      <div className="my-12 ">
+        <button className="btn btn-primary">Create Arrangement</button>
+      </div>
     </div>
   );
 }
